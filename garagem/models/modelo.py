261538721918a57f5e79ef74d4df19646f6ff16b
labs <gustavodep.gorges@gmail.com>
2023-08-25 10:13:57 -0300

    altered modelo view to show nome as title

--- garagem/models/modelo.py
+++ garagem/models/modelo.py
@@ -8,3 +8,6 @@ class Modelo(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="modelos")
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE, related_name="modelos")
     nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome.upper()
